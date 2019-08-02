7940ed9f752072a9d4445279557131936bff4803
SylvesterJS <49494833+SylvesterJS@users.noreply.github.com>
2019-08-02 20:32:54 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const output = document.querySelector(".output");
+const txt = document.querySelector("textarea");
+
+    ["keyup", "keydown", "change"].forEach(function(e){
+        txt.addEventListener(e, textCounter);
+    })
+    
+const maxLength = 120;
+const warnLength = 7;
+    
+    function textCounter(e){
+        let count = txt.value.length;
+        console.log(count);
+        if(count > maxLength){
+            txt.value = txt.value.substring(0, maxLength);
+        }
+        if(count > warnLength){
+            output.classList.add("red");
+        }else{
+            output.classList.remove("red");
+        }
+        output.innerHTML = "Pozostało " + (maxLength - count) + " znaków";
+    }
